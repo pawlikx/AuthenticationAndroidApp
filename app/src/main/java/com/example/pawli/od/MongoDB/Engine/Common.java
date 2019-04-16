9d057e106a1f34d231fdd395412002b44bfcc86b
pawlikx <pawlikmusic@gmail.com>
2019-04-16 21:17:00 +0200

    Update Common.java

--- app/src/main/java/com/example/pawli/od/MongoDB/Engine/Common.java
+++ app/src/main/java/com/example/pawli/od/MongoDB/Engine/Common.java
@@ -3,12 +3,12 @@ package com.example.pawli.od.MongoDB.Engine;
 import com.example.pawli.od.MongoDB.Classes.User;
 
 public class Common {
-    private static String DB_NAME = "alcobookdb";
+    private static String DB_NAME = "al..kdb";
 
     private static String COLLECTION_USERS = "test";
     private static String COLLECTION_RECIPES = "recipes";
     private static String COLLECTION_INGREDIENTS = "ingredients";
-    public static String API_KEY = "8CkaowtQyoLSSMDH1lSazBEsoZelcDj4";
+    public static String API_KEY = "8C...j4";
 
     public static String getAddressSingle(User user){
         String baseUrl = String.format("https://api.mlab.com/api/1/databases/%s/collections/%s", DB_NAME, COLLECTION_USERS);
